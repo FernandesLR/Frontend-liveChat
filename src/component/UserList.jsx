10e0a38620ce9a03353a0d7bf4e60760a50d9065
FernandesLr <leo1ramosf@gmail.com>
2024-11-07 19:51:45 -0300

    Style: ajuste no estilo da row de usuário

--- src/component/UserList.jsx
+++ src/component/UserList.jsx
@@ -1,8 +1,27 @@
+import avatar from '../assets/avatar.png'
 
 export function UserList(){
     return(
         <div className="rows">
-            
+            <div className="row">
+                <img src={avatar} alt="" style={{width: "5rem", borderRadius: "6px" }}/>
+
+                <div className="name-msg">
+                    <span>Nome</span>
+                    <p>ult. msg</p>
+                </div>
+                <p style={{color: 'rgb(183, 184, 185)', fontWeight: 'bold'}}>1m</p>
+            </div>
+
+            <div className="row">
+                <img src={avatar} alt="" style={{width: "5rem", borderRadius: "6px" }}/>
+
+                <div className="name-msg">
+                    <span>Nome</span>
+                    <p>ult. msg</p>
+                </div>
+                <p style={{color: 'rgb(183, 184, 185)', fontWeight: 'bold'}}>1m</p>
+            </div>
         </div>
     )
 }
